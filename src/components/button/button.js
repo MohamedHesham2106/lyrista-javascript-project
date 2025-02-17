6613f0cb0d954df6e40ddd9d3aa38700f230ab7b
Mohamed Hesham <moohisham1710@gmail.com>
2025-02-17 22:19:26 +0200

    feat
    boilerplate button and input

---
+++ src/components/button/button.js
@@ -0,0 +1,39 @@
+class Button extends HTMLElement {
+  constructor() {
+    super();
+    this.label = this.getAttribute("label") || "Submit";
+    this.type = this.getAttribute("type") || "button";
+    this.className = this.getAttribute("class") || "primary-button";
+    this.width = this.getAttribute("width") || "100%";
+    this.button = this.querySelector("button") || document.createElement("button");
+  }
+  connectedCallback() {
+    this.render();
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
+    const button = this.querySelector("button");
+    button.addEventListener("click", () => {
+      console.log("Button clicked");
+    });
+  }
+
+  render() {
+    this.button.textContent = this.label;
+    this.button.setAttribute("type", this.type);
+    this.button.setAttribute("class", this.className);
+    this.button.style.width = this.width;
+
+    this.removeAttribute("label");
+    this.removeAttribute("type");
+    this.removeAttribute("width");
+    this.removeAttribute("class");
+
+    if (!this.contains(this.button)) {
+      this.appendChild(this.button);
+    }
+  }
+}
+
+customElements.define("app-button", Button);
---
+++ src/components/input/input.js
@@ -0,0 +1,44 @@
+class Input extends HTMLElement {
+  constructor() {
+    super();
+    this.labelText = this.getAttribute("labelText") || "Text";
+    this.type = this.getAttribute("type") || "text";
+    this.setAttribute("class", "input-container");
+    this.width = this.getAttribute("width") || "100%";
+    this.name = this.getAttribute("name") || "";
+    this.input = this.querySelector("input") || document.createElement("input");
+    this.label = this.querySelector("label") || document.createElement("label");
+  }
+  connectedCallback() {
+    this.render();
+    this.setupEventListeners();
+  }
+  setupEventListeners() {
+    const input = this.querySelector("input");
+    input.addEventListener("input", () => {
+      console.log("Input changed");
+    });
+  }
+  render() {
+    this.label.textContent = this.labelText;
+    this.label.setAttribute("for", this.name);
+
+    this.input.setAttribute("type", this.type);
+    this.input.setAttribute("name", this.name);
+    this.input.setAttribute("id", this.name);
+    this.input.style.width = this.width;
+
+    this.removeAttribute("labelText");
+    this.removeAttribute("name");
+    this.removeAttribute("type");
+    this.removeAttribute("width");
+    if (!this.contains(this.label)) {
+      this.appendChild(this.label);
+    }
+    if (!this.contains(this.input)) {
+      this.appendChild(this.input);
+    }
+  }
+}
+
+customElements.define("app-input", Input);
