a47dbe0c5bdff12a682d3cacd03a8d2b929c0e4d
Islam-Tarek <islamtarek258@gmail.com>
2025-02-21 20:48:57 +0200

    updates for using the new components

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,32 +1,30 @@
-class Navbar extends HTMLElement{
+class Navbar extends HTMLElement {
 
-    constructor(){
+    constructor() {
         super();
         this.header = this.querySelector('header') || document.createElement('header');
         this.header.classList.add('header');
     }
 
-    connectedCallback(){
+    connectedCallback() {
         this.render();
         this.setupEventListeners();
     }
 
     setupEventListeners() {
         window.addEventListener("scroll", () => {
-          const navbar = this.querySelector(".header");
-          if (window.scrollY > 0) {
-            navbar.classList.add("scrolled");
-          } else {
-            navbar.classList.remove("scrolled");
-          }
+            const navbar = this.querySelector(".header");
+            if (window.scrollY > 0) {
+                navbar.classList.add("scrolled");
+            } else {
+                navbar.classList.remove("scrolled");
+            }
         });
-        this.header.querySelector(".fa-circle-user").addEventListener("click", () => {
-          const menu = this.header.querySelector(".menu");
-          menu.classList.toggle("hidden");
-        });
-      }
+        
+        
+    }
 
-    render(){
+    render() {
         const navbar = document.createElement('nav');
         navbar.classList.add('navbar');
 
@@ -34,25 +32,19 @@ class Navbar extends HTMLElement{
             <a href="/" class="logo"> My APP </a>
             <ul class="nav-list">
                 <li class="nav-link">
-                <app-button label="Search"></app-button>
-                </i>
-                <li class="nav-link">
-                    <i class="fa-solid fa-circle-user fa-2xl">
-                </i>
-                <div class="menu hidden">
-                    <div class="menu-item">Log out</div>
-                </div>
+                    <app-search></app-search>
+                </li>
+                <app-userbutton></app-userbutton>
             </ul>
         `;
-    
-        if(!this.header.contains(navbar)){
+
+        if (!this.header.contains(navbar)) {
             this.header.appendChild(navbar);
         }
-        if(!this.contains(this.header)){
+        if (!this.contains(this.header)) {
             this.appendChild(this.header);
         }
     }
-};
-
+}
 
 customElements.define("app-navbar", Navbar);
